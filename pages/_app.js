9ff7b85e7617a4ba06aad6073e7e9c1f5ddba90d
Jolien Vermaeren <jolienvermaeren@private.org>
2021-06-16 20:55:08 +0200

    testje

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp() {
+  return <h1>Hey</h1>
 }
-
-export default MyApp
+export default MyApp;
